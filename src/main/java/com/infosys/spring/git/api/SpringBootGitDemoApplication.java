35021b969d75d0699d642ef2fac43d70cf263932
Ravaskar <tv91lx@INSIM.BIZ>
2020-02-24 15:22:50 +0100

    added git-demo to local repo

---
+++ src/main/java/com/infosys/spring/git/api/SpringBootGitDemoApplication.java
@@ -0,0 +1,16 @@
+package com.infosys.spring.git.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootGitDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("*** Inside SpringBootGitDemoApplication ***");
+		SpringApplication.run(SpringBootGitDemoApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/infosys/spring/git/api/SpringBootGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.spring.git.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
